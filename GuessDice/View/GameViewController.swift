59b2f9726217e34298e0c6a7029be0a2571c7063
Özgün Can Beydili <ozguncanbeydili@icloud.com>
2023-07-28 21:25:18 +0300

    Desing part is done!

---
+++ GuessDice/View/GameViewController.swift
@@ -0,0 +1,32 @@
+//
+//  GameViewController.swift
+//  GuessDice
+//
+//  Created by Özgün Can Beydili on 28.07.2023.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    @IBOutlet weak var diceButton: UIButton!
+    @IBOutlet weak var firstDiceImageView: UIImageView!
+    @IBOutlet weak var secondDiceImageView: UIImageView!
+    @IBOutlet weak var predictTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var guessButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func diceButtonTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func guessButtonTapped(_ sender: Any) {
+        
+    }
+}
