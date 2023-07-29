fd943a6a3cabc6567d10d4b7c09aa6ea531bf9d9
Özgün Can Beydili <ozguncanbeydili@icloud.com>
2023-07-29 16:06:00 +0300

    Final state with fixed rights

--- GuessDice/View/GameViewController.swift
+++ GuessDice/View/GameViewController.swift
@@ -9,24 +9,111 @@ import UIKit
 
 class GameViewController: UIViewController {
 
-    @IBOutlet weak var diceButton: UIButton!
+    @IBOutlet weak var predictLabel: UILabel!
+    @IBOutlet weak var rollButton: UIButton!
     @IBOutlet weak var firstDiceImageView: UIImageView!
     @IBOutlet weak var secondDiceImageView: UIImageView!
     @IBOutlet weak var predictTextField: UITextField!
     @IBOutlet weak var resultLabel: UILabel!
     @IBOutlet weak var guessButton: UIButton!
+    @IBOutlet weak var againButton: UIButton!
+    
+    var remainingPredict = 5
+    var predict = 0
+    var result = 0
+    var firstRandom = 0
+    var secondRandom = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(closeKeyboard))
+        view.addGestureRecognizer(gestureRecognizer)
+        
+        resultLabel.text = ""
+        againButton.isHidden = true
     }
     
-    @IBAction func diceButtonTapped(_ sender: Any) {
-        
+    @objc func closeKeyboard() {
+        view.endEditing(true)
+    }
+    
+    @IBAction func rollButtonTapped(_ sender: Any) {
+        rollButton.isEnabled = false
+        firstRandom = Int.random(in: 1...6)
+        secondRandom = Int.random(in: 1...6)
+        result = firstRandom + secondRandom
+        print(firstRandom, secondRandom)
     }
     
     @IBAction func guessButtonTapped(_ sender: Any) {
-        
+        if rollButton.isEnabled == true {
+            let alert = UIAlertController(title: "!!!", message: "You must ROLL first!", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default))
+            present(alert, animated: true)
+        } else {
+            if let predict = Int(predictTextField.text!) {
+                remainingPredict -= 1
+                if predict == result {
+                    againButton.isHidden = false
+                    guessButton.isEnabled = false
+                    resultLabel.text = "Correct!"
+                    againButton.titleLabel?.text = "Play Again"
+                    predictLabel.text = "Remaining Predictions: \(remainingPredict + 1)"
+                    firstDiceImageView.image = showDice(firstRandom)
+                    secondDiceImageView.image = showDice(secondRandom)
+                } else {
+                    if remainingPredict > 0 {
+                        predictLabel.text = "Remaining Predictions: \(remainingPredict)"
+                        predictTextField.text = ""
+                    } else {
+                        againButton.isHidden = false
+                        predictLabel.text = "Remaining Predictions: \(remainingPredict)"
+                        resultLabel.text = "Wrong!"
+                        againButton.titleLabel?.text = "Try Again"
+                        firstDiceImageView.image = showDice(firstRandom)
+                        secondDiceImageView.image = showDice(secondRandom)
+                        guessButton.isEnabled = false
+                    }
+                }
+            }
+        }
+    }
+    
+    @IBAction func againButtonTapped(_ sender: Any) {
+        restart()
+    }
+    
+    func restart() {
+        rollButton.isEnabled = true
+        againButton.isHidden = true
+        guessButton.isEnabled = true
+        resultLabel.text = ""
+        remainingPredict = 5
+        predict = 0
+        result = 0
+        predictTextField.text = ""
+        predictLabel.text = "Remaining Predictions: \(remainingPredict)"
+        firstDiceImageView.image = UIImage(named: "undefineddice")
+        secondDiceImageView.image = UIImage(named: "undefineddice")
+    }
+    
+    func showDice(_ dice: Int) -> UIImage {
+        switch dice {
+        case 1:
+            return UIImage(named: "dice1")!
+        case 2:
+            return UIImage(named: "dice2")!
+        case 3:
+            return UIImage(named: "dice3")!
+        case 4:
+            return UIImage(named: "dice4")!
+        case 5:
+            return UIImage(named: "dice5")!
+        case 6:
+            return UIImage(named: "dice6")!
+        default:
+            return UIImage(named: "dice")!
+        }
     }
 }
